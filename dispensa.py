5724da866bf875d2c750a7991c57afa6bfd8ac65
JeffSSampaio <165072019+JeffSSampaio@users.noreply.github.com>
2024-05-20 09:36:12 -0300

    Add files via upload
    Criação da matriz dispensa

---
+++ dispensa.py
@@ -0,0 +1,11 @@
+from functions.functions import mostrarDispensa,verifyDispensaIsNotEmpty
+
+tam = 4
+linha = tam
+coluna = tam
+
+dispensa = [] * tam
+
+for l in range(len(dispensa)):
+    dispensa[l] = []* tam
+
