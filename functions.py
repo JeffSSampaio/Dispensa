43549bd62d891e5ee37083345f7cb6dd9fa80d66
JeffSSampaio <165072019+JeffSSampaio@users.noreply.github.com>
2024-05-20 09:52:28 -0300

    Update functions.py
    mudando o o "if cont >0"

--- functions.py
+++ functions.py
@@ -15,8 +15,10 @@ def verifyDispensaIsNotEmpty():
             if dispensa[l][c] is None or dispensa[l][c] == " ":
                   cont += 1
                   if cont > 0:
-                       print(f"Há {cont} espaço(s) vazio(s) na dispensa\nNas posições ({l},{c}) da dispensa ") 
-                  else:
+                    if cont > 0:
+                       print(f"Há espaço(s) vazio(s) na dispensa\nNas posições ({l},{c}) da dispensa ") 
+                       print(f"Há {cont} espaço(s) vazio(s)")
+                    else:
                        print("\n Não ha espaços vazios na dispensa")      
 
                   
