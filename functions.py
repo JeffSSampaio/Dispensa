707cf326fc3e982c9d3637d4d7bcf1b4af4d95c8
JeffSSampaio <165072019+JeffSSampaio@users.noreply.github.com>
2024-05-20 10:42:11 -0300

    Update functions.py
    alterando e pondo o def desconto

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 
 from dispensa import dispensa, tam , linha , coluna
-
+import random
 def mostrarDispensa():
      for l in range(len(dispensa)):
         print(dispensa[l])
@@ -21,7 +21,17 @@ def verifyDispensaIsNotEmpty():
                     else:
                        print("\n Não ha espaços vazios na dispensa")      
 
-                  
+ def dispararDesconto():
+      numAleatorio = random.uniform(1.0,10)
+      desconto = round(numAleatorio,2)
+      op = randint(0,1)
+       for l in range(len(dispensa)):
+           for c in range(len(dispensa[l])):
+                if op == 1:
+                     print(f"Item {dispensa[op][op]} por desconto de {desconto}%")
+                else:
+                     return
+                
         
 
 
