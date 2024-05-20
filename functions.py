25816c4ba82ba16407e57fb68eee9faa75104326
JeffSSampaio <165072019+JeffSSampaio@users.noreply.github.com>
2024-05-20 09:37:20 -0300

    Functions.py
    Funções para matriz dispensa.
    - função de mostrar matriz
    - função de dizer se matriz esta vazia

---
+++ functions.py
@@ -0,0 +1,25 @@
+
+from dispensa import dispensa, tam , linha , coluna
+
+def mostrarDispensa():
+     for l in range(len(dispensa)):
+        print(dispensa[l])
+
+def verifyDispensaIsNotEmpty():
+  if not dispensa or all(not linha for linha in dispensa): 
+              print("Dispensa esta vazia")
+              return
+  for l in range(len(dispensa)):
+    for c in range(len(dispensa[l])):
+            cont = 0
+            if dispensa[l][c] is None or dispensa[l][c] == " ":
+                  cont += 1
+                  if cont > 0:
+                       print(f"Há {cont} espaço(s) vazio(s) na dispensa\nNas posições ({l},{c}) da dispensa ") 
+                  else:
+                       print("\n Não ha espaços vazios na dispensa")      
+
+                  
+        
+
+
